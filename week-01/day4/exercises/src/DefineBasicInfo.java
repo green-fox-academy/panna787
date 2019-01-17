52a8fb5473af1fa8f67d27198a04e9290bb78e0f
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-17 14:33:12 +0100

    finish 10.ex. DefineBasicInfo

---
+++ week-01/day4/exercises/src/DefineBasicInfo.java
@@ -0,0 +1,18 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String name = "Aniko";
+        // Your age as an integer
+        int age = 31;
+        // Your height in meters as a double
+        double height = 1.75;
+        // Whether you are married or not as a boolean
+        Boolean married = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(married);
+    }
+}
