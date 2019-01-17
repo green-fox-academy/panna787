e49d3e7a5799507e29b6356cde17b64f97f89ff4
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-17 17:05:23 +0100

    finish 24.ex. PrintEven

--- week-01/day4/exercises/src/IWontCheatOnTheExams.java
+++ week-01/day4/exercises/src/IWontCheatOnTheExams.java
@@ -2,7 +2,7 @@ public class IWontCheatOnTheExams {
     public static void main(String[] args) {
 
         for (int j = 0;j <= 100;j++) {
-            System.out.println("I won't cheat on the exam!");
+            System.out.println(j + "I won't cheat on the exam!");
         }
     }
 }
---
+++ week-01/day4/exercises/src/PrintEven.java
@@ -0,0 +1,7 @@
+public class PrintEven {
+    public static void main(String[] args) {
+        for (int j = 0;j <= 500;j += 2) {
+            System.out.println(j);
+        }
+    }
+}
