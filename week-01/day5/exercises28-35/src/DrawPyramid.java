3e45be0c118eeb576e4db47a73b8556f0525ed89
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-18 14:37:45 +0100

    finish ex. 29. DrawPyramid

--- week-01/day5/exercises28-35/src/DrawPyramid.java
+++ week-01/day5/exercises28-35/src/DrawPyramid.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class DrawPyramid {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int lines = scanner.nextInt();
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < lines * 2; j++) {
+                if (j >= lines - i & j <= lines + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
