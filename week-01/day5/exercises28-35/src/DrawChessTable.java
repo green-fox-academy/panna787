57d83bc93c81cb129f5ad8c1d418c12fccf64947
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-18 14:24:34 +0100

    fnish ex. 35. DrawChessTable

--- week-01/day5/exercises28-35/src/DrawChessTable.java
+++ week-01/day5/exercises28-35/src/DrawChessTable.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class DrawChessTable {
+    public static void main(String[] args) {
+        int lines = 8;
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < lines; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) {
+                        System.out.print("%");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    if (j % 2 == 1) {
+                        System.out.print("%");
+                    } else {
+                        System.out.print(" ");
+                        }
+                    }
+            }
+            System.out.println();
+        }
+    }
 }
