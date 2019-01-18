049e5556ae183991b1784906dff79296de1cd621
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-18 15:36:33 +0100

    modified ParametricAverage

--- week-01/day5/exercises28-35/src/ParametricAverage.java
+++ week-01/day5/exercises28-35/src/ParametricAverage.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ParametricAverage {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int numberOfNumbers = scanner.nextInt();
