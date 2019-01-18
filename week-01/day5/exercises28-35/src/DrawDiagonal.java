e426d642bfb81db0280be3635076826b0f4cce4e
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-18 14:09:44 +0100

    finish ex. 32.

--- week-01/day5/exercises28-35/src/DrawDiagonal.java
+++ week-01/day5/exercises28-35/src/DrawDiagonal.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DrawDiagonal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int lines = scanner.nextInt();
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < lines; j++) {
+                if (i == 0 | i == lines-1) {
+                    System.out.print('%');
+                } else {
+                    if (j == 0 | j == lines - 1 | j == i) {
+                        System.out.print('%');
+                    } else {
+                        System.out.print(' ');
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
 }
