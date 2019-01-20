28d3fb2412b0b669ce0e36bb59a4cb23ce3568b6
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-20 20:25:57 +0100

    finish ex. 30. DrawDiamond

--- week-01/day5/exercises28-35/src/DrawDiamond.java
+++ week-01/day5/exercises28-35/src/DrawDiamond.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DrawDiamond {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int lines = scanner.nextInt();
+
+        if (lines % 2 == 1) {
+            int midLine = (lines + 1) / 2;
+
+            for (int i = 0; i < midLine; i++) {
+                for (int j = midLine - (i + 1); j > 0; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = i * 2 + 1; j > 0; j--) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for (int i = midLine; i < lines; i++) {
+                for (int j = 0; j < i - midLine + 1; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = (lines - i) * 2 - 1; j > 0; j--) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+
+        }
+
+
+    }
 }
