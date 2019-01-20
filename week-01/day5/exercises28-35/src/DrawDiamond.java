f784cdccbbe236aa46c5328637df1cbc52b242de
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-20 21:06:11 +0100

    Modify DrawDiamond for even numbers

--- week-01/day5/exercises28-35/src/DrawDiamond.java
+++ week-01/day5/exercises28-35/src/DrawDiamond.java
@@ -6,10 +6,10 @@ public class DrawDiamond {
         System.out.println("Enter a number: ");
         int lines = scanner.nextInt();
 
-        if (lines % 2 == 1) {
-            int midLine = (lines + 1) / 2;
 
-            for (int i = 0; i < midLine; i++) {
+        int midLine = (lines + 1) / 2;
+
+        for (int i = 0; i < midLine; i++) {
                 for (int j = midLine - (i + 1); j > 0; j--) {
                     System.out.print(" ");
                 }
@@ -17,7 +17,9 @@ public class DrawDiamond {
                     System.out.print("*");
                 }
                 System.out.println();
-            }
+        }
+
+        if (lines % 2 == 1) {
             for (int i = midLine; i < lines; i++) {
                 for (int j = 0; j < i - midLine + 1; j++) {
                     System.out.print(" ");
@@ -29,6 +31,17 @@ public class DrawDiamond {
                 System.out.println();
             }
 
+        } else {
+            for (int i = midLine; i < lines; i++) {
+                for (int j = 1; j < i - midLine + 1; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = (lines - i) * 2 - 1; j > 0; j--) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
         }
 
 
