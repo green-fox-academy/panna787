596cecda9344b9caafdde37908fbaad848cbfae5
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-21 13:01:59 +0100

    finish Doubling

---
+++ week-02/day1/functions-and-arrays/src/Doubllng.java
@@ -0,0 +1,15 @@
+public class Doubllng {
+
+    public static int baseNum = 123;
+
+    static int doubling(int num) {
+        int doubleNum = num * 2;
+
+        return doubleNum;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(doubling(baseNum));
+    }
+}
