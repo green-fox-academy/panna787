22e61d2bdaaa9cb424918a5d4aa4a8acdbecccb6
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-21 15:24:20 +0100

    finish DoubleItems, Colors, AppendA

---
+++ week-02/day1/functions-and-arrays/src/Colors.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Colors {
+
+    public static void main(String[] args) {
+
+        String[][] colors = new String[3][5];
+
+        colors[0] = new String[] {"lime", "forest green", "olive", "pale green", "spring green"};
+        colors[1] = new String[] {"orange red", "red", "tomato"};
+        colors[2] = new String[] {"orchid", "violet", "pink", "hot pink"};
+
+
+    }
+}
