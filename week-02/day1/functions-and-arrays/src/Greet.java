0b90f727996f5e0939fc763c29a56fa021483569
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-21 13:11:10 +0100

    finish Greet

---
+++ week-02/day1/functions-and-arrays/src/Greet.java
@@ -0,0 +1,15 @@
+public class Greet {
+
+    public static String a1 = "Greenfox";
+
+    public static void greet(String name) {
+
+        System.out.println("Greetings, dear " + name);
+    }
+
+    public static void main(String[] args) {
+
+        greet(a1);
+
+    }
+}
