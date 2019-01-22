bd8424defa74f9d51d2a35e83a9f0c9cf60c953e
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-22 12:04:48 +0100

    finish SimpleReplace, UrlFixer, TakesLonger

---
+++ week-02/day2/src/TakesLonger.java
@@ -0,0 +1,14 @@
+public class TakesLonger {
+    public static void main(String[] args) {
+        String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+        // When saving this quote a disk error has occured. Please fix it.
+        // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+        // Using pieces of the quote variable (instead of just redefining the string)
+
+        quote = quote.substring(0, quote.indexOf('y')) + "always takes longer than" + " " +
+                quote.substring(quote.indexOf('y'));
+
+        System.out.println(quote);
+    }
+}
