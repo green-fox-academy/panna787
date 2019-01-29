547cb8d534f0014492bb7e3d3b8e80992458ca1e
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-29 15:46:43 +0100

    WriteMultipleLines, CopyFile

---
+++ week-03/day2/src/CopyFile.java
@@ -0,0 +1,30 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+public class CopyFile {
+    public static void main(String[] args) {
+
+    }
+
+    public static Boolean copyFile(String fileNameCopyFrom, String fileNameCopyTo) {
+
+        try {
+
+            Path filepath1 = Paths.get(fileNameCopyFrom);
+            Path filepath2 = Paths.get(fileNameCopyTo);
+
+            ArrayList<String> linesToCopy = new ArrayList<>(Files.readAllLines(filepath1));
+
+            Files.write(filepath2, linesToCopy);
+
+            return true;
+        } catch (Exception ex) {
+
+            return false;
+
+        }
+
+    }
+}
