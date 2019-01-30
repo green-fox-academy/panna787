f59c18b943dc3aa121cef1d5f6f6a29989f66f43
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-30 11:14:45 +0100

    PostIts

---
+++ week-03/day3/src/postIt/PostIt.java
@@ -0,0 +1,15 @@
+package postIt;
+
+public class PostIt {
+    String backgroundColor;
+    String text;
+    String textColor;
+
+    public PostIt(String backgroundColor, String text, String textColor) {
+
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = text;
+
+    }
+}
---
+++ week-03/day3/src/postIt/PostIts.java
@@ -0,0 +1,12 @@
+package postIt;
+
+public class PostIts {
+
+    public static void main(String[] args) {
+
+        PostIt postIt1 = new PostIt("Orange", "Idea1", "Blue");
+        PostIt postIt2 = new PostIt("Pink", "Awesome", "Black");
+        PostIt postIt3 = new PostIt("Yellow", "Superb!", "Green");
+
+    }
+}
