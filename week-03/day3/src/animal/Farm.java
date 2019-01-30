9593cc8f76654766807b8045a10dfa73a4646807
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-30 17:18:18 +0100

    Farm

--- week-03/day3/src/animal/Farm.java
+++ week-03/day3/src/animal/Farm.java
@@ -1,4 +1,28 @@
 package animal;
 
+import java.util.ArrayList;
+
 public class Farm {
+
+    ArrayList<Animal> animals;
+    int freePlacesForNewAnimals;
+
+    public void breed(Animal newAnimal){
+        if (freePlacesForNewAnimals > 0) {
+            animals.add(newAnimal);
+            freePlacesForNewAnimals -= 1;
+        }
+    }
+
+    public void slaughter(){
+        Animal leastHungry = new Animal();
+
+        for (int i = 0; i < animals.size()-1; i++) {
+            if (animals.get(i).hunger < animals.get(i+1).hunger) {
+                leastHungry = animals.get(i);
+            }
+        }
+        animals.remove(leastHungry);
+        freePlacesForNewAnimals += 1;
+    }
 }
