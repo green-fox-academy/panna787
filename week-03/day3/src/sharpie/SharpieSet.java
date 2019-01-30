1c96dbfba0ced171b4fa64b874d532cae8214757
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-30 17:11:32 +0100

    SharpieSet

--- week-03/day3/src/sharpie/SharpieSet.java
+++ week-03/day3/src/sharpie/SharpieSet.java
@@ -1,4 +1,34 @@
 package sharpie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SharpieSet {
+
+    ArrayList<Sharpie> sharpieList;
+
+    public SharpieSet (ArrayList<Sharpie> sharpieList) {
+        this.sharpieList = sharpieList;
+    }
+
+    public int countUsable(){
+        int counter = 0;
+
+        for (Sharpie sharpie : sharpieList) {
+            if (sharpie.inkAmount > 0) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    public void removeTrash() {
+
+        for (Sharpie sharpie: sharpieList
+             ) {
+            if (sharpie.inkAmount < 1) {
+                sharpieList.remove(sharpie);
+            }
+        }
+    }
 }
