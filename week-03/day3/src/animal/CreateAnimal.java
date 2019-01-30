03d98bc610b3d10857a08bdc78c1bd7ea1e93ae8
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-30 11:42:17 +0100

    BlogPost, Animal and minor changes

---
+++ week-03/day3/src/animal/CreateAnimal.java
@@ -0,0 +1,18 @@
+package animal;
+
+public class CreateAnimal {
+
+    public static void main(String[] args) {
+
+        Animal pet = new Animal();
+
+        System.out.println(pet.hunger);
+
+        pet.play();
+
+        System.out.println(pet.hunger);
+    }
+
+
+
+}
---
+++ week-03/day3/src/blogpost/BlogPost.java
@@ -0,0 +1,19 @@
+package blogpost;
+
+public class BlogPost {
+
+    String authorName;
+    String title;
+    String text;
+    String publicationDate;
+
+    public BlogPost(String authorName, String title, String text, String publicationDate) {
+
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.publicationDate = publicationDate;
+
+    }
+
+}
