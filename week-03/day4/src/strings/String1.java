6ec0ac35bf535abbbb058089b6c7a9b2678cfe0c
Majoros Aniko <aniko.majoros87@gmail.com>
2019-01-31 14:34:00 +0100

    String1

--- week-03/day4/src/strings/String1.java
+++ week-03/day4/src/strings/String1.java
@@ -1,4 +1,24 @@
 package strings;
 
 public class String1 {
+
+    public static void main(String[] args) {
+        String chars = "lln-yoyxyáybhdoihgse";
+
+        System.out.println(xToY(chars));
+    }
+
+    public static String xToY(String chars) {
+        String stringToReturn = "";
+
+        if (chars.length() > 0) {
+            if(chars.endsWith("x")) {
+                chars = chars.substring(0, chars.length()-1);
+                stringToReturn = xToY(chars) + "y";
+            } else {
+                stringToReturn = xToY(chars.substring(0, chars.length()-1)) + chars.substring(chars.length()-1);
+            }
+        }
+        return stringToReturn;
+    }
 }
