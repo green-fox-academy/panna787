c9493aec3a3dd5662eceb5568b8f1dd42c06af9e
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-07 14:51:32 +0100

    finish Sum class and method

--- week-04/day4/src/Sum.java
+++ week-04/day4/src/Sum.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import sun.invoke.empty.Empty;
+
+import java.util.ArrayList;
 
 public class Sum {
+
+    public Integer sum(ArrayList<Integer> numbers){
+        Integer sum = 0;
+
+        if(numbers.equals(null)){
+            throw new NullPointerException("Null value not acceptable");
+        }
+        if(numbers.size() == 0){
+            throw new IllegalArgumentException("List is empty");
+        }
+        for (Integer number: numbers
+             ) {sum += number;
+
+        }
+    return sum;
+
+    }
 }
