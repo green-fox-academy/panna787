68c8431b9130890ce206404305429ef75188af46
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-12 11:41:43 +0100

    add Aircraft fields and base methods

--- week-04/day2/src/aircraftcarrier/Aircraft.java
+++ week-04/day2/src/aircraftcarrier/Aircraft.java
@@ -1,4 +1,44 @@
 package aircraftcarrier;
 
 public class Aircraft {
+
+    String type;
+    int maxAmmo;
+    int baseDamage;
+    int currentAmmo;
+    int allDamage;
+
+    public Aircraft(String type){
+        this.type = type;
+        this.maxAmmo = type.equals("F16") ? 8 : 12;
+        this.baseDamage = type.equals("F16") ? 30 : 50;
+        this.currentAmmo = 0;
+        this.allDamage = baseDamage * currentAmmo;
+    }
+
+    public int fight(){
+        allDamage = baseDamage * currentAmmo;
+        currentAmmo = 0;
+
+        return allDamage;
+    }
+
+    public int refill(int ammoToFill){
+        int remainingAmmo = 0;
+        if(ammoToFill + currentAmmo > maxAmmo){
+            currentAmmo = maxAmmo;
+            remainingAmmo = ammoToFill - maxAmmo;
+        } else {
+            currentAmmo += ammoToFill;
+        }
+        return remainingAmmo;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getStatus(){
+        String status = "Type: " + type + , Ammo: 10, Base Damage: 50, All Damage: 500
+    }
 }
---
+++ week-04/day2/src/aircraftcarrier/Carrier.java
@@ -0,0 +1,4 @@
+package aircraftcarrier;
+
+public class Carrier {
+}
