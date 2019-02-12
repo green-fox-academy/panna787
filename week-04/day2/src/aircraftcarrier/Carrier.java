d01458ea2ea893b1ee1ab81cce02672d4a6fff90
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-12 16:41:47 +0100

    add fill method

--- week-04/day2/src/aircraftcarrier/Carrier.java
+++ week-04/day2/src/aircraftcarrier/Carrier.java
@@ -23,21 +23,53 @@ public class Carrier {
 
         for (Aircraft aircraft: aircrafts
              ) {
-            if ((aircraft.isPriority())){
-                aircrafts.add(aircraft);
+            if (aircraft.isPriority()){
+                priorityAircrafts.add(aircraft);
             }
         }
         return priorityAircrafts;
     }
 
-    public void fill(){
+    public ArrayList<Aircraft> getNonPriorityAircrafts(){
+        ArrayList<Aircraft> nonPriorityAircrafts = new ArrayList<>();
+
+        for (Aircraft aircraft: aircrafts
+        ) {
+            if (!aircraft.isPriority()){
+                nonPriorityAircrafts.add(aircraft);
+            }
+        }
+        return nonPriorityAircrafts;
+    }
+
+    public int calculateAmmoNeeded(){
         int totalAmmoNeeded = 0;
         int ammoToFill = 0;
         for (Aircraft aircraft: aircrafts
-             ) {
+        ) {
             ammoToFill = aircraft.maxAmmo - aircraft.currentAmmo;
             totalAmmoNeeded += ammoToFill;
         }
-        
+
+        return totalAmmoNeeded;
+    }
+
+    public void fill(){
+        for (Aircraft aircraft: getPriorityAircrafts()
+             ) {
+            if(ammo == 0){
+                throw new ArithmeticException();
+            }
+            aircraft.refill(ammo);
+        }
+
+        for (Aircraft aircraft: getNonPriorityAircrafts()
+        ) {
+            if(ammo == 0){
+                throw new ArithmeticException();
+            }
+            aircraft.refill(ammo);
+        }
+
     }
 }
