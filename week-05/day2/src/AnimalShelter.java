4d50daee23a6531aef21f98f716c43409497f590
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-13 11:39:08 +0100

    add all methods to Animal Shelter

--- week-05/day2/src/AnimalShelter.java
+++ week-05/day2/src/AnimalShelter.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AnimalShelter {
 
-    int budget;
+    int budget = 50;
     List<Animal> animals = new ArrayList<>();
     List<String> adopters = new ArrayList<>();
 
@@ -37,5 +39,45 @@ public class AnimalShelter {
 
         return animalHealed;
     }
+
+    public void addAdopter(String name){
+        adopters.add(name);
+    }
+
+    public Map findNewOwner(){
+        Animal animalToAdopt = null;
+        String potentialOwner = "";
+        Map<String, Animal> ownerAndAnimal = new HashMap<>();
+
+        potentialOwner = adopters.get((int)Math.random() * (adopters.size()-1));
+        adopters.remove(potentialOwner);
+
+        while (!animalToAdopt.isAdoptable()){
+            animalToAdopt = animals.get((int)Math.random() * (animals.size()-1));
+        }
+
+        animals.remove(animalToAdopt);
+
+        ownerAndAnimal.put(potentialOwner, animalToAdopt);
+
+        return ownerAndAnimal;
+    }
+
+    public int earnDonation(int amount){
+        budget += amount;
+
+        return budget;
+    }
+
+    public String toString(){
+        String status = "Budget: " + budget + "€, There are " + animals.size() + " animal(s) and " + adopters.size() + " potential adopter(s)";
+
+        for (Animal a: animals
+             ) {
+            status += a.toString() + "/n";
+        }
+        return status;
+    }
+
 }
 
