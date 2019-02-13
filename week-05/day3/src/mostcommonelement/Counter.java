3f20045aad80e7c3732b5d34ef3a8abaf8dd5c60
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-13 12:23:12 +0100

    add first methods

---
+++ week-05/day3/src/mostcommonelement/Counter.java
@@ -0,0 +1,14 @@
+package mostcommonelement;
+
+import java.util.List;
+
+public class Counter {
+
+    public String findMostRainyCity(){
+
+    }
+
+    public List<String> getFileContent(){
+        
+    }
+}
