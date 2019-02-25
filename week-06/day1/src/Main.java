87c4bbb3a17d62998f3a056a78b856502a67b580
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-25 12:27:11 +0100

    finish exercise1

--- week-06/day1/src/Main.java
+++ week-06/day1/src/Main.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+        List<Integer> evenNumbers = numbers.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
+
+
+    }
 }
