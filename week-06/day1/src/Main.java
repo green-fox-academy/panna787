e973b3d8fd39fb9770279bf04a6278dc00a3ddc0
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-25 12:33:25 +0100

    finish exercise 2, 3

--- week-06/day1/src/Main.java
+++ week-06/day1/src/Main.java
@@ -8,7 +8,16 @@ public class Main {
 
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
+        //Exercise 1
         List<Integer> evenNumbers = numbers.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
+
+        //Exercise 2
+        List<Integer> squaredNumbers = numbers.stream().filter(n -> n>0).map(n -> n*n).collect(Collectors.toList());
+
+        //Exercise 3
+        List<Integer> numbers2 = Arrays.asList(3, 9, 2, 8, 6, 5);
+        List<Integer> squareAbove20 = numbers2.stream().filter(n -> n*n > 20).collect(Collectors.toList());
+
 
 
     }
