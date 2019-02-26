77d0a45a2fc44f7f6e774fe0b27edd1517be3713
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-26 16:00:50 +0100

    create random greetings

--- week-06/day2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -18,4 +18,27 @@ public class HelloWebController {
         model.addAttribute("count", id.getAndIncrement());
         return "greeting";
     }
+
+    @RequestMapping(value="/web/greetingworld")
+    public String greetingWorld(Model model){
+        String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+
+        String hello = hellos[(int)(Math.random()*(hellos.length-1))];
+        int fontSize = (int)(Math.random()*40)+14;
+        int color1 = (int)(Math.random()*255);
+        int color2 = (int)(Math.random()*255);
+        int color3 = (int)(Math.random()*255);
+
+        model.addAttribute("hello", hello);
+        model.addAttribute("fontsize", fontSize);
+        model.addAttribute("color1", color1);
+        model.addAttribute("color2", color2);
+        model.addAttribute("color3", color3);
+
+        return "greeting2";
+    }
 }
