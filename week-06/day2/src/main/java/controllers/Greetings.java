98d7db4d09b772d0c74e3240efaf9a4d38dc3168
Majoros Aniko <aniko.majoros87@gmail.com>
2019-02-26 11:39:26 +0100

    create controllers package and Greetings class

---
+++ week-06/day2/src/main/java/controllers/Greetings.java
@@ -0,0 +1,20 @@
+package controllers;
+
+public class Greetings {
+
+    private long id;
+    private String content;
+
+    public Greetings(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
--- week-06/day2/src/test/java/com/greenfoxacademy/springstart/SpringstartApplicationTests.java
+++ week-06/day2/src/test/java/com/greenfoxacademy/springstart/SpringstartApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringstartApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
