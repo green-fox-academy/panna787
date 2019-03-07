f27c6f356f940c7c09c94f9095cad760f68eb441
Majoros Aniko <aniko.majoros87@gmail.com>
2019-03-07 16:56:11 +0100

    finish Colored Background exercise

--- week-07/day4/src/main/java/com/greenfoxacademy/dependencies/controllers/UtilityController.java
+++ week-07/day4/src/main/java/com/greenfoxacademy/dependencies/controllers/UtilityController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.dependencies.controllers;
 import com.greenfoxacademy.dependencies.models.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,11 @@ public class UtilityController {
     @RequestMapping("/useful")
     public String main(){
         return "index";
+    }
+
+    @RequestMapping("/useful/colored")
+    public String colorBackground(Model model){
+        model.addAttribute("color", u.randomColor());
+        return "empty";
     }
 }
