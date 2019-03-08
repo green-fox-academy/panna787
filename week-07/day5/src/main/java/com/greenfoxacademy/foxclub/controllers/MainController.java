1c3c3562a3645445f1423248e649146115668458
Majoros Aniko <aniko.majoros87@gmail.com>
2019-03-08 14:17:12 +0100

    create login post method, add trick list to index

--- week-07/day5/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day5/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,18 +1,57 @@
 package com.greenfoxacademy.foxclub.controllers;
 
+import com.greenfoxacademy.foxclub.models.Fox;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
-    public String main(){
-        return "index";
+    private List<Fox> foxes = new ArrayList<>();
+
+    public MainController(){
+        foxes.add(new Fox("fox1", "food1", "drink1"));
+        foxes.add(new Fox("fox2", "food2", "drink2"));
+        foxes.add(new Fox("fox3", "food3", "drink3"));
     }
 
-    @RequestMapping("/login")
-    public String login(){
+    @RequestMapping("/")
+    public String main(Model model,@RequestParam(name="name") String name){
+        Fox fox = null;
+        for (Fox f: foxes) {
+            if(name.equals(f.getName())){
+                fox = f;
+            }
+        }
+        if(fox != null){
+            model.addAttribute("fox", fox);
+            return "index";
+
+        } else {
+            return "login";
+        }
+
+    }
+
+    @RequestMapping(value = "/login", method= RequestMethod.GET)
+    public String loginForm(){
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method= RequestMethod.POST)
+    public String login(@ModelAttribute(name="fox") Fox fox, @RequestParam String name){
+
+        return "redirect:/?name=" + name;
+
+
     }
 }
