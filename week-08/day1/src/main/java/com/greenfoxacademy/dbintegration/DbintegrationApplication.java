8154113860f454bde90a457dd9586ecc1187af3d
Majoros Aniko <aniko.majoros87@gmail.com>
2019-03-11 11:36:53 +0100

    create project

---
+++ week-08/day1/src/main/java/com/greenfoxacademy/dbintegration/DbintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.dbintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbintegrationApplication.class, args);
+    }
+
+}
