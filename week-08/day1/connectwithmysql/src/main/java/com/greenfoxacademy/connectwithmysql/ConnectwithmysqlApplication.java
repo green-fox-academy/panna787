5fc4e0dbd3b49e0acbcc0d99c7ee15fe78a3f828
Majoros Aniko <aniko.majoros87@gmail.com>
2019-03-11 12:35:57 +0100

    move todo project, create mysql project

---
+++ week-08/day1/connectwithmysql/src/main/java/com/greenfoxacademy/connectwithmysql/ConnectwithmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.connectwithmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectwithmysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectwithmysqlApplication.class, args);
+    }
+
+}
