78dabb1cb04804b6540ef3c24b20f4c96b70781a
Majoros Aniko <aniko.majoros87@gmail.com>
2019-03-13 21:16:49 +0100

    create Assignee class

---
+++ week-08/day3/todoextended/src/main/java/com/greenfoxacademy/dbintegration/models/Assignee.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.dbintegration.models;
+
+
+import javax.persistence.Entity;
+
+@Entity
+public class Assignee {
+
+    private String name;
+    private String email;
+}
