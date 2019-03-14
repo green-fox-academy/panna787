647e4da791c5e228d329ab91b17eb674c2913d28
Majoros Aniko <aniko.majoros87@gmail.com>
2019-03-14 09:53:49 +0100

    add packages and Post class

---
+++ week-08/day4/src/main/java/com/greenfoxacademy/reddit/models/Post.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.reddit.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String url;
+}
